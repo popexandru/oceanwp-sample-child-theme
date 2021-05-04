ee861d0fc05c02464ef5393528f0251a544ff3f4
popexandru <alexandru.popescu86@gmail.com>
2021-05-04 16:56:14 +0300

    Added new plugin s360-less

--- includes/scripts.php
+++ includes/scripts.php
@@ -45,7 +45,7 @@
      */
 
     add_action( 'wp_enqueue_scripts', function(){
-    wp_register_script( 's360-scripts', get_stylesheet_directory_uri() . '/public/js/scripts.js', [ 'jquery'/*, 'masonry'*/ ], null, true );
+        wp_register_script( 's360-scripts', get_stylesheet_directory_uri() . '/public/js/scripts.js', [ 'jquery'/*, 'masonry'*/ ], null, true );
         wp_enqueue_script( 's360-scripts' );
 
         /*wp_register_script( 's360-slick', get_stylesheet_directory_uri() . '/assets/js/slick.js', [ 'jquery' ], null, true );
